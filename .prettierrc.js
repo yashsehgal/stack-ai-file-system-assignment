f4511642a5e3da5ad6281f5fa3e8c5efd2f8103d
Yash Sehgal <yashsehgal.work@gmail.com>
2024-11-16 21:30:14 +0530

    chore: create next app

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSameLine: true,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'always',
+  printWidth: 140,
+};
